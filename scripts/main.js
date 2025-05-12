973907caee0a715631ae3cc751fe4a36776cc750
JustineBiancaMStaRomana <justinebianca.staromana@smc.pshs.edu.ph>
2025-05-12 19:03:17 +0800

    New build

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+}
+  
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+
+function toTop() {
+  window.scrollTo(0,0);
+}
+
+function buttonAppear(){
+  document.getElementById("uppies").style.opacity = 0.95;
+}
+
+function buttonDisappear(){
+  document.getElementById("uppies").style.opacity = 0;
+}
